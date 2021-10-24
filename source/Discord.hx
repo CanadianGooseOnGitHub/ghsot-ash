b3e92bcc2237dc536deb3f0d788eaf3e937227ca
CrystalSlime <56366422+CrystalSlime@users.noreply.github.com>
2021-10-24 15:29:37 -0400

    ash

--- source/Discord.hx
+++ source/Discord.hx
@@ -11,7 +11,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "901912487010639942",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
